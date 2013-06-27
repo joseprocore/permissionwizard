27d10c2dfad913c8b69fc4ff923073f96771ed2e
TJKresch <tjkresch@gmail.com>
2013-06-27 13:44:11 -0700

    tab pages added content

--- app/helpers/wizard_helper.rb
+++ app/helpers/wizard_helper.rb
@@ -3,4 +3,16 @@ module WizardHelper
 		params[:action]
 	end
 
+	def current_tab_title
+		if params[:action] == 'rfis'
+			'RFIs'
+		else
+			title = ''
+			params[:action].humanize.split.each do |word|
+				title << word.capitalize
+				title << ' '
+			end
+			title.strip
+		end
+	end
 end
